5f21fb914fe632c49a303c97c79fc9491d8893e0
Yury Antonov <1390348+yantonov@users.noreply.github.com>
2023-04-01 14:17:55 +0200

    ignore symbolic links for executable autodetection, consider only regular files as candidates for execution

--- src/environment/autodetect_executable.rs
+++ src/environment/autodetect_executable.rs
@@ -1,9 +1,9 @@
 use std::env;
-use std::path::{PathBuf, Path};
+use std::path::Path;
 
 pub fn autodetect_executable(executable_path: &Path,
                              executable_name: &str,
-                             checker: &dyn CheckFile) -> Option<String> {
+                             fs: &dyn FileSystemWrapper) -> Option<String> {
     let executable_name_as_path = Path::new(executable_name);
     match env::var("PATH") {
         Ok(path_var) => {
@@ -18,9 +18,10 @@ pub fn autodetect_executable(executable_path: &Path,
                     let target_path = Path::join(
                         &path_item,
                         &executable_name_as_path);
-                    if checker.exists(&target_path) {
-                        let string = target_path.as_os_str().to_str().unwrap().to_string();
-                        return Some(string);
+                    if fs.exists(&target_path) {
+                        if fs.is_file(&target_path) {
+                            return Some(target_path.to_str().unwrap().to_string());
+                        }
                     }
                 }
             }
@@ -30,33 +31,22 @@ pub fn autodetect_executable(executable_path: &Path,
     }
 }
 
-pub trait CheckFile {
+pub trait FileSystemWrapper {
     fn exists(&self, path: &Path) -> bool;
+    fn is_file(&self, path: &Path) -> bool;
 }
 
-pub struct OsCheckFile {}
+pub struct OsFileSystemWrapper {}
 
-impl CheckFile for OsCheckFile {
+impl FileSystemWrapper for OsFileSystemWrapper {
     fn exists(&self, path: &Path) -> bool {
         path.exists()
     }
-}
 
-struct DummyCheckFile {
-    expected_path: PathBuf,
-}
-
-impl CheckFile for DummyCheckFile {
-    fn exists(&self, path: &Path) -> bool {
-        self.expected_path == path
-    }
-}
-
-struct NoFile {}
-
-impl CheckFile for NoFile {
-    fn exists(&self, _path: &Path) -> bool {
-        false
+    fn is_file(&self, path: &Path) -> bool {
+        let metadata = std::fs::symlink_metadata(path);
+        return metadata.map(|x| x.is_file())
+            .unwrap_or(false);
     }
 }
 
@@ -66,23 +56,69 @@ mod tests {
 
     use super::*;
     use std::path::Path;
+    use std::collections::HashMap;
+
+    #[derive(Clone)]
+    struct TestFileDescriptor {
+        is_file: bool,
+    }
+
+    impl TestFileDescriptor {
+        pub fn file() -> TestFileDescriptor {
+            return TestFileDescriptor {
+                is_file: true
+            };
+        }
+    }
+
+    struct TestFileSystemWrapper {
+        path_to_descriptor: HashMap<String, TestFileDescriptor>,
+    }
+
+    impl TestFileSystemWrapper {
+        pub fn create() -> TestFileSystemWrapper {
+            return TestFileSystemWrapper {
+                path_to_descriptor: HashMap::new()
+            };
+        }
+
+        pub fn add(&mut self, path: &str, descriptor: &TestFileDescriptor) {
+            self.path_to_descriptor.insert(path.to_string(), (*descriptor).clone());
+        }
+    }
+
+    impl FileSystemWrapper for TestFileSystemWrapper {
+        fn exists(&self, path: &Path) -> bool {
+            self.path_to_descriptor.contains_key(path.to_str().unwrap())
+        }
+
+        fn is_file(&self, path: &Path) -> bool {
+            return self.path_to_descriptor.get(path.to_str().unwrap())
+                .map(|d| d.is_file)
+                .unwrap_or(false);
+        }
+    }
 
     #[test]
     fn target_executable_can_be_found_later_in_the_path() {
+        let mut fs = TestFileSystemWrapper::create();
+        fs.add("/bin/alias", &TestFileDescriptor::file());
+        fs.add("/usr/bin/alias", &TestFileDescriptor::file());
         set_path(vec![
             "/bin",
             "/usr/bin"]);
         let autodetect = autodetect_executable(
             Path::new("/bin"),
             "alias",
-            &DummyCheckFile {
-                expected_path: Path::new("/usr/bin/alias").to_owned()
-            }).unwrap();
+            &fs).unwrap();
         assert_eq!("/usr/bin/alias", autodetect);
     }
 
     #[test]
     fn target_executable_cannot_be_found_later_in_the_path() {
+        let mut fs = TestFileSystemWrapper::create();
+        fs.add("/home/username/alias_app/alias", &TestFileDescriptor::file());
+        fs.add("/home/username/some_app/alias", &TestFileDescriptor::file());
         set_path(vec![
             "/home/username/some_app",
             "/home/username/alias_app",
@@ -91,24 +127,27 @@ mod tests {
         assert!(autodetect_executable(
             Path::new("/home/username/alias_app"),
             "alias",
-            &NoFile {})
+            &fs)
             .is_none());
     }
 
     #[test]
-    fn alias_path_does_not_exist_in_path_wait_autodetect_executable_fail() {
+    fn alias_path_does_not_exist_in_path() {
+        let fs = TestFileSystemWrapper::create();
         set_path(vec![
             "/bin",
             "/usr/bin"]);
         assert!(autodetect_executable(
             Path::new("/home/username/app"),
             "alias",
-            &NoFile {})
+            &fs)
             .is_none());
     }
 
     #[test]
-    fn alias_path_exists_but_target_executable_doesnt_expect_autodetect_fail() {
+    fn alias_path_exists_but_target_executable_doesnt() {
+        let mut fs = TestFileSystemWrapper::create();
+        fs.add("/home/username/app/alias", &TestFileDescriptor::file());
         set_path(vec![
             "/home/username/app",
             "/bin",
@@ -116,7 +155,7 @@ mod tests {
         assert!(autodetect_executable(
             Path::new("/home/username/app"),
             "alias",
-            &NoFile {})
+            &fs)
             .is_none());
     }
 
