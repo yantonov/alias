0f2d74521140bb1e0aa786fc1cff7e3ad953b753
Yury Antonov <1390348+yantonov@users.noreply.github.com>
2026-08-22 13:07:34 +0200

    cargo handles it, no need to do strip manually

--- bin/dev/release.sh
+++ bin/dev/release.sh
@@ -9,9 +9,3 @@ TARGET="$(pwd)/target/release/${EXECUTABLE_NAME}"
 cargo build --release
 
 echo "binary file is here: ${TARGET}"
-
-if command -v strip &> /dev/null
-then
-    # reduce binary size
-    strip "${TARGET}"
-fi
